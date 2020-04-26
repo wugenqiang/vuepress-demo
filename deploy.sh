a3530c3f9a48acd4b373740d63c8448a0fbdaea0
WuGenQiang <2422676183@qq.com>
2020-04-26 22:21:24 +0800

    auto update https://github.com/wugenqiang/vuepress-demo.git

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,13 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run build
-
 git init
 git add -A
 git commit -m 'auto update https://github.com/wugenqiang/vuepress-demo.git'
 git push -f https://github.com/wugenqiang/vuepress-demo.git master
 
+# 生成静态文件
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
